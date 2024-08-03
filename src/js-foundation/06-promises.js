8baf94061195c68158f57695e5bf41bbd17eaca6
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-03 10:19:34 -0500

    feat: Implementación de async y await

--- src/js-foundation/06-promises.js
+++ src/js-foundation/06-promises.js
@@ -1,11 +1,18 @@
 
-const getPokemonById = ( id ) => {
+const getPokemonById = async( id ) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${ id }`
 
-  return fetch( url )
-    .then((resp) => resp.json())
-    // .then(() => { throw new Error('Pokemon no encontrado') })
-    .then((pokemon) => pokemon.name)
+  const resp = await fetch( url )
+  const pokemon = await resp.json()
+
+  // throw new Error('Pokemon no existe')
+
+  return pokemon.name
+
+  // return fetch( url )
+  //   .then((resp) => resp.json())
+  //   // .then(() => { throw new Error('Pokemon no encontrado') })
+  //   .then((pokemon) => pokemon.name)
 }
 
 module.exports = {
