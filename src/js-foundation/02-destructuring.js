2f5b84d5edf1a3aa071c5c9990158fd82b51d79a
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-03 08:42:07 -0500

    Desestructuración de objetos y arreglos

---
+++ src/js-foundation/02-destructuring.js
@@ -0,0 +1,10 @@
+// console.log( process.env )
+
+const { USERNAME, windir } = process.env
+
+// console.table({ USERNAME, windir })
+
+const characters = ['Naruto', 'Sasuke', 'Kakashi']
+const [ , , kakashi ] = characters
+
+// console.log(kakashi)
