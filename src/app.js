f5af0bee4de26f735a59c4272a5913902c0f7018
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-03 09:04:01 -0500

    callbacks

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-const { emailTemplate } = require('./js-foundation/01-template');
-require('./js-foundation/02-destructuring')
+// const { emailTemplate } = require('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring')
+ const { getUserByID } = require('./js-foundation/03-callbacks')
 
 // console.log(emailTemplate);
+
+const id = 1
+
+getUserByID( id, function(err, user) {
+  if(err) throw new Error(err)
+
+    console.log({ user })
+} )
