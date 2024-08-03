fffbeb5e7c0366e8e44b27f174b5e59afaf53631
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-03 09:09:58 -0500

    Funciones flecha

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring')
- const { getUserByID } = require('./js-foundation/03-callbacks')
+//  const { getUserByID } = require('./js-foundation/03-callbacks')
+const { getUserByID } = require('./js-foundation/04-arrow')
 
 // console.log(emailTemplate);
 
 const id = 1
 
-getUserByID( id, function(err, user) {
-  if(err) throw new Error(err)
+getUserByID(id, (err, user) => {
+  if (err) throw new Error(err)
 
-    console.log({ user })
-} )
+  console.log({ user })
+})
